3242f65ba74f0b8bda36d6ab5e4225d8f2dfaeb0
Wendy Leon <pixelmamma@gmail.com>
2020-08-10 00:40:19 -0400

    Adding file 1.5

---
+++ week-1/nodejs-in-action.js
@@ -0,0 +1,34 @@
+/* ============================================
+; Title: Assignment 1.4 Node.js Server
+; Author: Professor Massoud
+; Date: 8 August 2020
+; Modified By: Wendy Leon
+; Description: Web Server
+;=========================================== */
+
+var header = require('../header.js');
+
+console.log(header.display("Wendy", "Leon", "Exercise 1.4 Servers"));
+console.log('\n');
+
+// start program
+
+var http = require("http");
+
+function processRequest(req, res) {
+
+  var body = "Allo tout le monde";
+
+  var contentLength = body.length;
+
+  res.writeHead(200, {
+    "Content-Length": contentLength,
+    "Content-Type": "text/plain",
+  });
+  res.end(body);
+}
+var s = http.createServer(processRequest);
+
+s.listen(8080);
+
+// end programnode
