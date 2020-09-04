c4c4e8adca04ba54bb65278b513fed46bc133b2b
Wendy Leon <pixelmamma@gmail.com>
2020-09-03 23:10:05 -0400

    Adding  files

---
+++ week-5/if-else-render/leon-assignment-5.2.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title:  EJS Templates
+; Author: Professor Krasso
+; Modified by: Wendy Leon
+; Date:   3 Sept 2020
+; Description: EJS Templates
+;===========================================
+*/
+//header
+var header = require('../../header.js');
+console.log(header.display('Wendy', 'Leon', 'Exercise 5.2 - EJS Templates'));
+console.log('');
+
+// start program
+
+var express = require("express");
+var http = require ("http");
+var path = require ("path");
+
+var app = express();
+// template file
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+//people array
+var names = ["Paola", 
+              "Eva", 
+              "Cassey", 
+              "Daniela"
+              ];
+
+app.get("/", function (request, response){
+    response.render ("index", {people: names});
+});
+
+http.createServer(app).listen(8000, function(){
+    console.log("Application started / listening on port 8000");
+});
+
+//end program
