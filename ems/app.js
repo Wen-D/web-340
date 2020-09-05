0c3b2f9c9543866af90e255be697203cd76981ca
Wendy Leon <pixelmamma@gmail.com>
2020-09-04 22:53:26 -0400

    Adding 5.4

---
+++ ems/app.js
@@ -0,0 +1,39 @@
+/*
+============================================
+; Title:  5.4 UI Dev
+; Author: Professor Krasso
+; Modified by: Wendy Leon
+; Date:   4 Sept 2020
+; Description: 5.4 UI Dev
+;===========================================
+*/
+
+//header
+
+var header = require('../../header.js');
+console.log(header.display('Wendy', 'Leon', 'Exercise 5.4 UI Dev'));
+console.log('');
+
+// start program
+
+var express = require("express");
+var http = require("http");
+var path = require ("path");
+var logger = require ("morgan");
+var app = express();
+
+// template folder
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(logger("short"));
+
+//template 
+app.get("/", function (request, response){
+    response.render("index", {title: "Home Page"});
+});
+
+http.createServer(app).listen(8080, function(){console.log("Application started on port 8080!")});
+
+
+//end program
