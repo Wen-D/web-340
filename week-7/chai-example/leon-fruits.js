141c929623f499117666a62ff7a7174eeb6e1978
Wendy Leon <pixelmamma@gmail.com>
2020-09-20 23:42:15 -0400

    Adding 7.3

---
+++ week-7/chai-example/leon-fruits.js
@@ -0,0 +1,18 @@
+/*
+=========================================
+; Title:  Mocha and Chai
+; Author: Professor Krasso
+; Modified by: Wendy Leon
+; Date:  Sept 20 2020
+; Description: Mocha and Chai
+;=======================================
+*/
+
+function fruits(str) {
+
+    return str.split(',');
+
+}
+
+module.exports = fruits;
+
